5622030178fb85d9730f61453ea697152bc15904
James Pier <jpier2012@gmail.com>
2019-05-23 16:18:54 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,4 +6,12 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     where('rating = ?', highest_rating)[0]
   end
+
+  def self.lowest_rating
+    minimum(:rating)
+  end
+
+  def self.least_popular_show
+    where('rating = ?', lowest_rating)[0]
+  end
 end
